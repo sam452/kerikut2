2536f080ab1aa16054ec2d12eb4c6b2f24633afb
sam walton <sam@foofrancine.com>
2012-02-23 07:55:40 -0600

    factory working

--- features/step_definitions/user_views_corn.rb
+++ features/step_definitions/user_views_corn.rb
@@ -1,8 +1,8 @@
 Given /^a price for corn has already been entered$/ do
-	corn = Commodity.create!(name: "Corn", wholesale_unit: "ton", retail_unit: "gallon",  farm_gate_unit: "crate", delivered_unit: "bushel")
- 	Price.create!(commodity: corn, quality: "good", price_type: "government", farm_gate_high: "12", farm_gate_low: "10", delivered_high: "9", delivered_low: "7", wholesale_high: "14", wholesale_low: "13", retail_high: "19", retail_low: "17", date: "01/01/1990")
- 	#corn = Factory(:user)
- 	#Factory(:price)
+	#corn = Commodity.create!(name: "Corn", wholesale_unit: "ton", retail_unit: "gallon",  farm_gate_unit: "crate", delivered_unit: "bushel")
+ 	#Price.create!(commodity: corn, quality: "good", price_type: "government", farm_gate_high: "12", farm_gate_low: "10", delivered_high: "9", delivered_low: "7", wholesale_high: "14", wholesale_low: "13", retail_high: "19", retail_low: "17", date: "01/01/1990")
+ 	corn = Factory(:commodity)
+ 	Factory(:price)
 end
 
 When /^I go to the home\s?page$/ do
@@ -10,7 +10,3 @@ When /^I go to the home\s?page$/ do
 end
 
 
-Then /^I should not see "([^"]*)" in Retail low \(gallon\)$/ do | |
-save_and_open_page
-  pending # express the regexp above with the code you wish you had
-end

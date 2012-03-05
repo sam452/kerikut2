7bb097ec3f6e49e8d810692594675154c278a0cf
sam walton <sam@foofrancine.com>
2012-03-05 10:17:48 -0600

    useful factory def

--- features/support/factories.rb
+++ features/support/factories.rb
@@ -2,28 +2,28 @@ require 'factory_girl'
 
 FactoryGirl.define do
   
-  factory :commodity do |f|
-    f.name 'Corn'
-    f.wholesale_unit 'ton'
-    f.retail_unit 'gallon'
-    f.farm_gate_unit 'crate'
-    f.delivered_unit 'bushel'
-    f.receipt_unit 'dollar'
+  factory :commodity do
+    name 'Corn'
+    wholesale_unit 'ton'
+    #retail_unit 'gallon'
+    #farm_gate_unit 'crate'
+    #delivered_unit 'bushel'
+    #receipt_unit 'dollar'
   end
   
-  factory :price do |f|
-    f.quality 'good'
-    f.price_type 'government'
-    f.farm_gate_high '12#{n}'
-    f.farm_gate_low '10'
-    f.delivered_high '9'
-    f.delivered_low '7'
-    f.wholesale_high '14'
-    f.wholesale_low '13'
-    f.retail_high '19'
-    f.retail_low '17'
-    f.date '01/01/1990'
-    association :commodity, :factory => :commodity
+  factory :price do 
+    quality 'good'
+    price_type 'government'
+    sequence(:farm_gate_high) { |n| '12#{n}' }
+    #sequence(:farm_gate_low) { |n| '10#{n}' }
+    #sequence(:delivered_high) { |n| '9#{n}' }
+    #sequence(:delivered_low) { |n| '7#{n}' }
+    #sequence(:wholesale_high) { |n| '14#{n}' }
+    #sequence(:wholesale_low) { |n| '13#{n}' }
+    #sequence(:retail_high) { |n| '19#{n}' }
+    #sequence(:retail_low) { |n| '17#{n}' }
+    #sequence(:date) { |n| '01/0#{n}/1990' }
+    commodity
   end
   
 end	
